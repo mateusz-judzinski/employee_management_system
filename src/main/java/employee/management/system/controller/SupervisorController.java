cc3ffaef8f1b1a0fbada1b3572c4633fcb272fe3
Mateusz Judziński <mateusz.judzinski@gmail.com>
2024-12-11 21:32:37 +0100

    some work with supervisor

--- src/main/java/employee/management/system/controller/SupervisorController.java
+++ src/main/java/employee/management/system/controller/SupervisorController.java
@@ -1,11 +1,15 @@
 package employee.management.system.controller;
 
+import employee.management.system.entity.Employee;
 import employee.management.system.service.EmployeeService;
 import employee.management.system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/supervisor-panel")
@@ -25,4 +29,11 @@ public class SupervisorController {
         return "supervisor-panel-page";
     }
 
+    @GetMapping("/employees")
+    public String showEmployeesPage(Model model){
+        List<Employee> employees = employeeService.getAllEmployees();
+        model.addAttribute("employees", employees);
+
+        return "all-employees-page";
+    }
 }
