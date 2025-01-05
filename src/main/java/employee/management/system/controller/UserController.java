fa8b803f72aa9f0cde4e4766b1bc8b5426b98cd6
Mateusz Judziński <mateusz.judzinski@gmail.com>
2025-01-05 20:51:58 +0100

    trying to add some temporary position feature

--- src/main/java/employee/management/system/controller/UserController.java
+++ src/main/java/employee/management/system/controller/UserController.java
@@ -16,13 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UserController {
 
-    private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping("/")
     public String getHomePage(){
         return "user/home";
