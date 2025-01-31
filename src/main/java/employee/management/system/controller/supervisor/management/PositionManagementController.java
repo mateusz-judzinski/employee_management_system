b35c4d43eb2085953c885b568b8daaa2b2cee8e7
Mateusz Judziński <mateusz.judzinski@gmail.com>
2025-01-31 14:15:35 +0100

    minor changes into shift controller - removed repetition

--- src/main/java/employee/management/system/controller/supervisor/management/PositionManagementController.java
+++ src/main/java/employee/management/system/controller/supervisor/management/PositionManagementController.java
@@ -1,7 +1,9 @@
 package employee.management.system.controller.supervisor.management;
 
 import employee.management.system.entity.Position;
+import employee.management.system.entity.Qualification;
 import employee.management.system.entity.Skill;
+import employee.management.system.service.QualificationService;
 import employee.management.system.service.SkillService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +17,11 @@ import java.util.List;
 public class PositionManagementController {
 
     private final SkillService skillService;
+    private final QualificationService qualificationService;
 
-    public PositionManagementController(SkillService skillService) {
+    public PositionManagementController(SkillService skillService, QualificationService qualificationService) {
         this.skillService = skillService;
+        this.qualificationService = qualificationService;
     }
 
     @GetMapping("/add-position")
@@ -28,6 +32,9 @@ public class PositionManagementController {
 
         List<Skill> skill = skillService.findAllSkills();
         model.addAttribute("skills", skill);
+
+        List<Qualification> qualifications = qualificationService.findAllQualifications();
+        model.addAttribute("qualifications", qualifications);
 
         return "supervisor/positions/form";
     }
--- src/main/java/employee/management/system/controller/supervisor/management/ShiftManagementController.java
+++ src/main/java/employee/management/system/controller/supervisor/management/ShiftManagementController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -44,7 +45,7 @@ public class ShiftManagementController {
     }
 
     @GetMapping("/add-shift")
-    public String showShiftForm(Model model) {
+    public String showShiftForm(Model model, @RequestParam(value = "errorMessage", required = false) String errorMessage) {
 
         Shift shift = new Shift();
         List<Employee> employees = employeeService.getAllEmployeesSortedByLastName();
@@ -52,11 +53,15 @@ public class ShiftManagementController {
         model.addAttribute("shift", shift);
         model.addAttribute("employees", employees);
 
+        if (errorMessage != null) {
+            model.addAttribute("errorMessage", errorMessage);
+        }
+
         return "supervisor/shifts/form";
     }
 
     @PostMapping("/add-shift")
-    public String addShift(@ModelAttribute("shift") Shift shift, Model model) {
+    public String addShift(@ModelAttribute("shift") Shift shift, RedirectAttributes redirectAttributes) {
 
         LocalDate shiftDate = shift.getWorkDate();
         LocalDate now = LocalDate.now();
@@ -66,30 +71,18 @@ public class ShiftManagementController {
 
         if (alreadyHaveShiftThatDay) {
             String errorMessage = "Pracownik posiada już przypisaną zmianę w podany dzień";
-            model.addAttribute("errorMessage", errorMessage);
+            redirectAttributes.addAttribute("errorMessage", errorMessage);
 
-            Shift newShift = new Shift();
-            List<Employee> employees = employeeService.getAllEmployeesSortedByLastName();
-
-            model.addAttribute("shift", newShift);
-            model.addAttribute("employees", employees);
-
-            return "supervisor/shifts/form";
+            return "redirect:/supervisor-panel/management/add-shift";
 
         } else if (shiftDate.isBefore(now) ||
                 (shiftDate.isEqual(now) &&
                         shiftEndTime.isBefore(LocalTime.now()) &&
                         shiftEndTime.isAfter(shift.getStartTime()))) {
+
             String errorMessage = "Nie można dodawać zmian na przeszłe dni";
-            model.addAttribute("errorMessage", errorMessage);
-
-            Shift newShift = new Shift();
-            List<Employee> employees = employeeService.getAllEmployeesSortedByLastName();
-
-            model.addAttribute("shift", newShift);
-            model.addAttribute("employees", employees);
-
-            return "supervisor/shifts/form";
+            redirectAttributes.addAttribute("errorMessage", errorMessage);
+            return "redirect:/supervisor-panel/management/add-shift";
         }
 
         Employee employee = employeeService.findEmployeeById(shiftEmployee.getId());
